d4ddcf34a19652e465c716d48357e593b0d8a18b
chen <23054115+cwkang1998@users.noreply.github.com>
2021-11-14 00:36:06 +0800

    feat: Implement simple wave portal contract

---
+++ scripts/run.ts
@@ -0,0 +1,35 @@
+/* eslint-disable no-process-exit */
+import { ethers } from "hardhat";
+
+const main = async () => {
+  const [owner, randomPerson] = await ethers.getSigners();
+
+  const waveContractFactory = await ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy();
+  await waveContract.deployed();
+
+  console.log("Contract deployed to: ", waveContract.address);
+  console.log("Contract deployed by: ", owner.address);
+
+  let waveCount = await waveContract.getTotalWaves();
+  let waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
+  waveTxn = await waveContract.connect(randomPerson).wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+runMain();
